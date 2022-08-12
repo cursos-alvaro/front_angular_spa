91fe337a4b9e5af13e6c03d745f883c1fd47269b
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2022-08-12 00:03:45 -0300

    feat: Criação dos módulos funcionais e configuração inicial das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    title: "Meus Games - Home"
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/my-games/my-games.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyGamesRoutingModule } from './my-games-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MyGamesRoutingModule
+  ]
+})
+export class MyGamesModule { }
